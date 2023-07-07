ec5d0b3352482841ce9569dfe7642382499204a9
KW <kee.wei26@gmail.com>
2023-07-07 23:30:36 +0100

    done extended criteria, left refactoring

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class CharacterPoints {
     String characters;
     int point;
@@ -11,6 +14,10 @@ class CharacterPoints {
 
 public class Scrabble {
     String scrabbleString;
+    Character[] doubleLetters;
+    Character[] tripleLetters;
+    boolean doubleWord;
+    boolean tripleWord;
     static final CharacterPoints[] charPoints = {
             new CharacterPoints("AEIOULNRST", 1),
             new CharacterPoints("DG", 2),
@@ -25,6 +32,26 @@ public class Scrabble {
         this.scrabbleString = word;
     }
 
+    public Scrabble(String word, Character[] doubleLetter, Character[] tripleLetter, boolean doubleWord, boolean tripleWord) {
+        this.scrabbleString = word;
+        this.doubleLetters = doubleLetter;
+        this.tripleLetters = tripleLetter;
+        this.doubleWord = doubleWord;
+        this.tripleWord = tripleWord;
+    }
+
+    private boolean isNullOrEmpty() {
+        if(scrabbleString == null || scrabbleString.trim().isEmpty()) {
+            return true;
+        }
+        return false;
+    };
+
+    /**
+     *
+     * @param letter
+     * @return array of points corresponding to each letter in the string
+     */
     private int getPoints(char letter) {
         for(CharacterPoints charAndPoints : charPoints) {
             char upperCaseLetter = Character.toUpperCase(letter);
@@ -36,23 +63,95 @@ public class Scrabble {
         return 0;
     }
 
-    private boolean isNullOrEmpty() {
-        if(scrabbleString == null || scrabbleString.trim().isEmpty()) {
-            return true;
+    /**
+     *
+     * @return array of first index of the character in the scrabbleString
+     *
+     *          if doubleLetters is null/length 0, return -1
+     */
+    private ArrayList<Integer> getDoubleLettersIndex() {
+        if (doubleLetters == null || doubleLetters.length == 0 ) {
+            return new ArrayList<>(Arrays.asList(-1));
         }
-        return false;
+        ArrayList<Integer> indexes = new ArrayList<Integer> ();
+        for(char c : doubleLetters) {
+            indexes.add(scrabbleString.indexOf(c));
+        }
+
+        return indexes;
     };
+
+    /**
+     *
+     * @return array of indexes of character in the scrabbleString
+     *         if the letter is in doubleLetters, find the next index of the char in scrabbleString
+     *         else, add the first index to array
+     *
+     *         if tripleLetter is not given / length is 0, return -1
+     */
+    private ArrayList<Integer> getTripleLettersIndex() {
+        if (tripleLetters == null || tripleLetters.length == 0 ) {
+            return new ArrayList<>(Arrays.asList(-1));
+        }
+
+        ArrayList<Integer> indexes = new ArrayList<Integer> ();
+
+        for(char c : tripleLetters) {
+            int firstInd = scrabbleString.indexOf(c);
+
+            if(Arrays.stream(doubleLetters).anyMatch(letter -> letter == c)) {
+                indexes.add(scrabbleString.indexOf(c, firstInd + 1));
+                continue;
+            }
+            indexes.add(scrabbleString.indexOf(c));
+        }
+        return indexes;
+    }
+
+    /**
+     *
+     * @return array of points corresponding to the position of the letters in scrabbleString
+     */
+    private ArrayList<Integer> stringToPoints() {
+        ArrayList<Integer> eachCharPoint = new ArrayList<Integer>();
+
+        for (int i = 0; i < scrabbleString.length(); i++) {
+            eachCharPoint.add(getPoints(scrabbleString.charAt(i)));
+        }
+        return eachCharPoint;
+    }
+
+    private int calculateTotalWithBonuses() {
+        ArrayList<Integer> stringToPoints = stringToPoints();
+        ArrayList<Integer> doubleIndexes = getDoubleLettersIndex();
+        ArrayList<Integer> tripleIndexes = getTripleLettersIndex();
+
+        int total = 0;
+
+        for (int i = 0; i < stringToPoints.size() ; i++) {
+            if (!doubleIndexes.contains(-1) && doubleIndexes.contains(i)) {
+                total += stringToPoints.get(i) * 2;
+                continue;
+            }
+            if (!tripleIndexes.contains(-1) && tripleIndexes.contains(i)) {
+                total += stringToPoints.get(i) * 3;
+                continue;
+            }
+
+            total += stringToPoints.get(i);
+        }
+
+        return total;
+    }
+
 
     public int score() {
         if(isNullOrEmpty()) return 0;
 
-        int totalPoint = 0;
+        scrabbleString = scrabbleString.toUpperCase();
+        int totalPoint = calculateTotalWithBonuses();
 
-        for (int i = 0; i < scrabbleString.length(); i++) {
-            totalPoint += getPoints(scrabbleString.charAt(i));
-        }
-
-        return totalPoint;
+        return doubleWord ? totalPoint * 2 : tripleWord ? totalPoint * 3 : totalPoint;
     }
 }
 
--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -48,55 +48,51 @@ public class ScrabbleTest {
         assertEquals(scrabble.score(), 41);
     }
 
-//    @Test
+    @Test
+    public void returnsScoreForDoubleWord() {
+        Scrabble scrabble = new Scrabble("quirky", new Character[]{}, new Character[]{}, true, false);
+        assertEquals(scrabble.score(), 44);
+    }
+
+    @Test
+    public void returnsScoreForTripleWord() {
+        Scrabble scrabble = new Scrabble("quirky", new Character[]{}, new Character[]{}, false, true);
+        assertEquals(scrabble.score(), 66);
+    }
+
+    @Test
+    public void returnsScoreForDoubleLetterWord() {
+        Scrabble scrabble = new Scrabble("a", new Character[]{'A'}, new Character[]{}, false, false);
+        assertEquals(scrabble.score(), 2);
+    }
+
+    @Test
 //    @Disabled
-//    public void returnsScoreForDoubleWord() {
-//        // Scrabble (String, Character[], Character[], boolean (doubleWord), boolean(tripleWord);
-//        Scrabble scrabble = new Scrabble("quirky", new Character[]{}, new Character[]{}, true, false);
-//        assertEquals(scrabble.score(), 44);
-//    }
-//
-//    @Test
+    public void returnsScoreForDoubleLetterOnlyOnce() {
+        Scrabble scrabble = new Scrabble("aa", new Character[]{'A'}, new Character[]{}, false, false);
+        assertEquals(scrabble.score(), 3);
+    }
+
+    @Test
 //    @Disabled
-//    public void returnsScoreForTripleWord() {
-//        Scrabble scrabble = new Scrabble("quirky", new Character[]{}, new Character[]{}, false, true);
-//        assertEquals(scrabble.score(), 66);
-//    }
-//    // Scrabble (String, Character[] (double letters), Character[], boolean, boolean);
-//    @Test
+    public void returnsScoreForTripleLetterWord() {
+        // Scrabble (String, Character[] , Character[] (triple letters), boolean, boolean;
+        Scrabble scrabble = new Scrabble("a", new Character[]{}, new Character[]{'A'},false, false);
+        assertEquals(scrabble.score(), 3);
+    }
+
+    @Test
 //    @Disabled
-//    public void returnsScoreForDoubleLetterWord() {
-//        Scrabble scrabble = new Scrabble("a", new Character[]{'A'}, new Character[]{}, false, false);
-//        assertEquals(scrabble.score(), 2);
-//    }
-//
-//    @Test
+    public void returnsScoreForTripleLetterOnlyOnce() {
+        Scrabble scrabble = new Scrabble("aa", new Character[]{}, new Character[]{'A'},false, false);
+        assertEquals(scrabble.score(), 4);
+    }
+
+    @Test
 //    @Disabled
-//    public void returnsScoreForDoubleLetterOnlyOnce() {
-//        Scrabble scrabble = new Scrabble("aa", new Character[]{'A'}, new Character[]{}, false, false);
-//        assertEquals(scrabble.score(), 3);
-//    }
-//
-//    @Test
-//    @Disabled
-//    public void returnsScoreForTripleLetterWord() {
-//        // Scrabble (String, Character[] , Character[] (triple letters), boolean, boolean;
-//        Scrabble scrabble = new Scrabble("a", new Character[]{}, new Character[]{'A'},false, false);
-//        assertEquals(scrabble.score(), 3);
-//    }
-//
-//    @Test
-//    @Disabled
-//    public void returnsScoreForTripleLetterOnlyOnce() {
-//        Scrabble scrabble = new Scrabble("aa", new Character[]{}, new Character[]{'A'},false, false);
-//        assertEquals(scrabble.score(), 4);
-//    }
-//
-//    @Test
-//    @Disabled
-//    public void returnsScoreForDoubleAndTripleLetter() {
-//        Scrabble scrabble = new Scrabble("aa", new Character[]{'A'}, new Character[]{'A'},false, false);
-//        assertEquals(scrabble.score(), 5);
-//    }
+    public void returnsScoreForDoubleAndTripleLetter() {
+        Scrabble scrabble = new Scrabble("aa", new Character[]{'A'}, new Character[]{'A'},false, false);
+        assertEquals(scrabble.score(), 5);
+    }
 
 }
